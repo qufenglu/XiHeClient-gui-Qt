7494d4f0d1bf7959fd231165f20fc8902d8d8c4b
星海 <38244437+qufenglu@users.noreply.github.com>
2024-05-27 10:04:22 +0800

    Update Log.cpp
    日志模块问题修复

--- XiHeClient/BaseClass/Log/Log.cpp
+++ XiHeClient/BaseClass/Log/Log.cpp
@@ -4,19 +4,22 @@
 #include <chrono>
 #include <ratio>
 #include <iostream>
+#include <mutex>
 #include "Log.h"
 
 FILE* g_pLogFile = nullptr;
 LogLevel g_eLogLevel = DEBUG;
+std::mutex g_cLogLock;
 
 int32_t InitLog(std::string path)
 {
+    std::lock_guard<std::mutex> lock(g_cLogLock);
     if (g_pLogFile != nullptr)
     {
         return -1;
     }
 
-    g_pLogFile = freopen(path.c_str(), "ab+", stdout);
+    g_pLogFile = fopen(path.c_str(), "ab+");
     if (g_pLogFile == nullptr)
     {
         return -2;
@@ -27,6 +30,7 @@ int32_t InitLog(std::string path)
 
 int32_t UnInitLog()
 {
+    std::lock_guard<std::mutex> lock(g_cLogLock);
     if (g_pLogFile == nullptr)
     {
         return 0;
@@ -41,21 +45,13 @@ int32_t UnInitLog()
 
 int32_t GetTimeStr(char* buff, size_t size)
 {
-    /*time_t curentTime;
-    time(&curentTime);
-    tm* pCurentTime = localtime(&curentTime);
-    if (0 == strftime(buff, size, "%Y-%m-%d %T", pCurentTime))
-    {
-        return -1;
-    }*/
-
     auto now = std::chrono::system_clock::now();
     uint64_t disMillseconds = std::chrono::duration_cast<std::chrono::milliseconds>(now.time_since_epoch()).count()
         - std::chrono::duration_cast<std::chrono::seconds>(now.time_since_epoch()).count() * 1000;
     time_t tt = std::chrono::system_clock::to_time_t(now);
     auto time_tm = localtime(&tt);
 
-    snprintf(buff,size,"%d-%02d-%02d %02d:%02d:%02d.%03d", time_tm->tm_year + 1900,
+    snprintf(buff, size, "%d-%02d-%02d %02d:%02d:%02d.%03d", time_tm->tm_year + 1900,
         time_tm->tm_mon + 1, time_tm->tm_mday, time_tm->tm_hour,
         time_tm->tm_min, time_tm->tm_sec, (int)disMillseconds);
 
@@ -75,17 +71,20 @@ void Error(const char* format, ...)
         return;
     }
 
-    char buff[64];
-    GetTimeStr(buff, 64);
-    std::cout << buff << " [E] ";
+    {
+        char buff[64];
+        GetTimeStr(buff, 64);
 
-    va_list list;
-    va_start(list, 1);
-    vprintf(format, list);
-    printf("\n");
-    va_end(list);
+        std::lock_guard<std::mutex> lock(g_cLogLock);
+        g_pLogFile == nullptr ? printf("%s%s", buff, " [Error] ") : fprintf(g_pLogFile, "%s%s", buff, " [Error] ");
 
-    std::cout.flush();
+        va_list args;
+        va_start(args, format);
+        g_pLogFile == nullptr ? vprintf(format, args) : vfprintf(g_pLogFile, format, args);
+        va_end(args);
+
+        g_pLogFile == nullptr ? printf("\r\n") : fprintf(g_pLogFile, "\r\n");
+    }
 }
 
 void Warn(const char* format, ...)
@@ -95,17 +94,20 @@ void Warn(const char* format, ...)
         return;
     }
 
-    char buff[64];
-    GetTimeStr(buff, 64);
-    std::cout << buff << " [W] ";
+    {
+        char buff[64];
+        GetTimeStr(buff, 64);
 
-    va_list list;
-    va_start(list, 1);
-    vprintf(format, list);
-    printf("\n");
-    va_end(list);
+        std::lock_guard<std::mutex> lock(g_cLogLock);
+        g_pLogFile == nullptr ? printf("%s%s", buff, " [Warn] ") : fprintf(g_pLogFile, "%s%s", buff, " [Warn] ");
 
-    std::cout.flush();
+        va_list args;
+        va_start(args, format);
+        g_pLogFile == nullptr ? vprintf(format, args) : vfprintf(g_pLogFile, format, args);
+        va_end(args);
+
+        g_pLogFile == nullptr ? printf("\r\n") : fprintf(g_pLogFile, "\r\n");
+    }
 }
 
 void Trace(const char* format, ...)
@@ -115,17 +117,20 @@ void Trace(const char* format, ...)
         return;
     }
 
-    char buff[64];
-    GetTimeStr(buff, 64);
-    std::cout << buff << " [T] ";
+    {
+        char buff[64];
+        GetTimeStr(buff, 64);
 
-    va_list list;
-    va_start(list, 1);
-    vprintf(format, list);
-    printf("\n");
-    va_end(list);
+        std::lock_guard<std::mutex> lock(g_cLogLock);
+        g_pLogFile == nullptr ? printf("%s%s", buff, " [Trace] ") : fprintf(g_pLogFile, "%s%s", buff, " [Trace] ");
 
-    std::cout.flush();
+        va_list args;
+        va_start(args, format);
+        g_pLogFile == nullptr ? vprintf(format, args) : vfprintf(g_pLogFile, format, args);
+        va_end(args);
+
+        g_pLogFile == nullptr ? printf("\r\n") : fprintf(g_pLogFile, "\r\n");
+    }
 }
 
 void Debug(const char* format, ...)
@@ -135,17 +140,20 @@ void Debug(const char* format, ...)
         return;
     }
 
-    char buff[64];
-    GetTimeStr(buff, 64);
-    std::cout << buff << " [D] ";
+    {
+        char buff[64];
+        GetTimeStr(buff, 64);
 
-    va_list list;
-    va_start(list, 1);
-    vprintf(format, list);
-    printf("\n");
-    va_end(list);
+        std::lock_guard<std::mutex> lock(g_cLogLock);
+        g_pLogFile == nullptr ? printf("%s%s", buff, " [Debug] ") : fprintf(g_pLogFile, "%s%s", buff, " [Debug] ");
 
-    std::cout.flush();
+        va_list args;
+        va_start(args, format);
+        g_pLogFile == nullptr ? vprintf(format, args) : vfprintf(g_pLogFile, format, args);
+        va_end(args);
+
+        g_pLogFile == nullptr ? printf("\r\n") : fprintf(g_pLogFile, "\r\n");
+    }
 }
 
 void Panic(const char* format, ...)
@@ -155,15 +163,18 @@ void Panic(const char* format, ...)
         return;
     }
 
-    char buff[64];
-    GetTimeStr(buff, 64);
-    std::cout << buff << " [P] ";
+    {
+        char buff[64];
+        GetTimeStr(buff, 64);
 
-    va_list list;
-    va_start(list, 1);
-    vprintf(format, list);
-    printf("\n");
-    va_end(list);
+        std::lock_guard<std::mutex> lock(g_cLogLock);
+        g_pLogFile == nullptr ? printf("%s%s", buff, " [Panic] ") : fprintf(g_pLogFile, "%s%s", buff, " [Panic] ");
 
-    std::cout.flush();
+        va_list args;
+        va_start(args, format);
+        g_pLogFile == nullptr ? vprintf(format, args) : vfprintf(g_pLogFile, format, args);
+        va_end(args);
+
+        g_pLogFile == nullptr ? printf("\r\n") : fprintf(g_pLogFile, "\r\n");
+    }
 }
